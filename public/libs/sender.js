440fca7ccad84ddd8936c5b2f5b6b9b94a1b7a67
belljonathan50 <belljonathan50@gmail.com>
2021-09-08 16:38:51 +0200

    Create sender.js

---
+++ public/libs/sender.js
@@ -0,0 +1,18 @@
+function myFunction() {
+  bubu.onclick = function() {
+    console.log("start");
+    var text = "start";
+    ws.send(text);
+  }
+}
+
+
+function myFunction2() {
+  toto.onclick = function() {
+    console.log("stop");
+    var text = "stop";
+    ws.send(text);
+  }
+}
+
+
