962368a2d7ebe46832b8a46e88317c0851212c12
belljonathan50 <belljonathan50@gmail.com>
2021-09-09 21:48:03 +0200

    Update receive.js

--- public/libs/receive.js
+++ public/libs/receive.js
@@ -12,47 +12,58 @@
     var first = words[0];
     var seekValue = parseInt(words[1]);
 
-    console.log(msg);
-    console.log("response "+ response);
-    console.log("words "+ words);
-    console.log("first "+ first);
-    console.log("seekValue "+ seekValue);
-    console.log(typeof seekValue);
-    console.log(typeof seekValue);
-    console.log(typeof seekValue);
+    // console.log(msg);
+    // console.log("response "+ response);
+    // console.log("words "+ words);
+    // console.log("first "+ first);
+    // console.log("seekValue "+ seekValue);
+    // console.log(typeof seekValue);
+    // console.log(typeof seekValue);
+    // console.log(typeof seekValue);
     
 
 
     switch (first) {
+
       case "start":
         inscore.postMessageStrStr ("/ITL/scene", "event", "START0");
         break;
+
       case "pause":
         inscore.postMessageStrStr ("/ITL/scene", "event", "STOP0");
         break;
-      case "seek":
 
+      case "seek":
         var message = inscore.newMessageM ("date");
         inscore.msgAddF (message, seekValue);
         inscore.postMessage ("/ITL/scene/cursor", message); 
-
-        // inscore.postMessageStrStr ("/ITL/scene/cursor", "date", "3");
-
-
-        // /ITL/scene/cursor date 0,
-
         break;
+
       case "dummy":
         console.log("dummy");
         break;
+
       case "startX":
         inscore.postMessageStrStr ("/ITL/scene", "event", "STARTX");
         break;
+
       case "stop":
         inscore.postMessageStrStr ("/ITL/scene", "event", "REWIND");
         break;
-      case  6:
-        day = "Saturday";
+
+      case  "hit":
+        var NowClientSide = new Date(ts.now());
+        var forsee = words[4];
+        var difference = forsee - NowClientSide;
+        console.log("NowClientSIde "+ NowClientSide.getTime());
+        console.log("forsee "+ forsee);
+        console.log("difference "+ difference);
+        setTimeout( function() {inscore.postMessageStrStr("/ITL/scene", "event", "STARTX")}, difference)
+        break;
+
+        case "DelayPlay":
+          console.log("del " + words[1])
+
     }
 
 
