342847a91279588e59e0e491671ecdcc39561b6e
belljonathan50 <belljonathan50@gmail.com>
2021-09-18 11:34:18 +0200

    Create receivevid.js

---
+++ public/libs/receivevid.js
@@ -0,0 +1,83 @@
+(function() {
+  var HOST = location.origin.replace(/^http/, 'ws');
+  var ws = new WebSocket(HOST);
+  
+  var form = document.querySelector('.form');
+
+  var vid = document.getElementById('vid');
+
+
+  
+  ws.onmessage = function(msg) {
+    var response = msg.data;
+    console.log(response);
+    var words = response.split(' ');
+    var first = words[0];
+    var seekValue = parseInt(words[1]);
+    var vid = document.getElementById('vid');
+
+    // console.log(msg);
+    // console.log("response "+ response);
+    // console.log("words "+ words);
+    // console.log("first "+ first);
+    // console.log("seekValue "+ seekValue);
+    // console.log(typeof seekValue);
+    // console.log(typeof seekValue);
+    // console.log(typeof seekValue);
+    
+
+
+    switch (first) {
+      
+      case "start":
+        vid.currentTime = 0;
+        vid.play();
+        
+        break;
+
+      case "pause":
+        vid.pause();
+        break;
+
+      case "seek":
+        vid.currentTime = seekValue;
+        break;
+
+      case "dummy":
+        console.log("dummy");
+        break;
+
+      case "startX":
+        vid.play();
+        break;
+
+      case "stop":
+        vid.pause();
+        vid.currentTime = 0;
+        break;
+
+      case  "hit":
+        var NowClientSide = new Date(ts.now());
+        var forsee = words[4];
+        var difference = forsee - NowClientSide;
+        console.log("NowClientSIde "+ NowClientSide.getTime());
+        console.log("forsee "+ forsee);
+        console.log("difference "+ difference);
+        setTimeout( function() { vid.play(); }, difference)
+        break;
+
+        // case  "write":
+        //   console.log("receive the write ");
+       
+        //   document.write("hello world")
+        //   break;
+          
+
+    }
+
+
+
+
+
+  }
+}());
